939659852763ca0b3d9fa30011868488cda24367
Yash Kataria <yashkataria14.yk@gmail.com>
2020-07-21 18:56:36 +0530

    Created server.py and client.py
    Server.py listens for requests and on succesful connection with client, it displays the output of command on client machine entered from server side.
    
    Client.py executes the command and returns the output of command entered from server side.

---
+++ src/client.py
@@ -0,0 +1,21 @@
+import socket
+import subprocess
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5003
+BUFFER_SIZE = 1024
+
+s = socket.socket()
+s.connect((SERVER_HOST, SERVER_PORT))
+
+message = s.recv(BUFFER_SIZE).decode()
+print("Server:", message)
+
+while True:
+    command = s.recv(BUFFER_SIZE).decode()
+    if command.lower() == "exit":
+        break
+    output = subprocess.getoutput(command)
+    s.send(output.encode())
+
+s.close()
